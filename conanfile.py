2c774941183011cdae6b65db3d6a0a63df661c5d
Matthias Zink <matthias-zink@gmx.net>
2020-04-30 22:00:08 +0200

    Update conan recipe.

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,25 @@
-import subprocess
-
+from pathlib import Path
 from conans import ConanFile, CMake, tools
 
 class Clang(ConanFile):
-    name = 'clang_10'
-    description = 'Compiler for debian desktop'
-    license = 'MIT'
-    version = '1.0.0'
-    generators = 'cmake', 'virtualenv'
-    settings = 'os', 'arch', 'compiler', 'build_type'
+    name="clang_10"
+    description="Compiler for debian desktop"
+    version="1.0.0"
+    url="https://github.com/matz1/conan_clang_10.git"
+    generators="virtualenv"
+    settings="os"
 
     def source(self):
         tools.get("https://github.com/llvm/llvm-project/archive/llvmorg-10.0.0.tar.gz")
-        self.run("mv llvm-project-llvmorg-10.0.0/* .")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=self.source_folder + '/llvm',
-          args=['-DLLVM_ENABLE_PROJECTS=clang', '-DCMAKE_BUILD_TYPE=Release', '../llvm'])
+        cmake.configure(
+          source_folder=str(Path(self.source_folder) / "llvm-project-llvmorg-10.0.0/llvm"),
+          args=["-DLLVM_ENABLE_PROJECTS=clang", "-DCMAKE_BUILD_TYPE=Release"]
+        )
         cmake.build()
         cmake.install()
 
     def package_info(self):
-        self.env_info.path.append(self.package_folder + '/' + self.name + '/bin')
+        self.env_info.path.append(str(Path(self.package_folder) / "bin"))
